1e8fff65ea6aaf3dfc79bd7368e15397437528c1
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-09-21 12:19:17 +0300

    header and footer

--- register.php
+++ register.php
@@ -1,15 +1,6 @@
-<?php  include("myfunctions.php")?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Admin-User System</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="">
-</head>
-<body>
-    <div class="container">
+<?php  include("myfunctions.php"); ?>
+<?php  include("header.php"); ?>
+
         <div class="form-holder">
             <form method="POST" action="register.php">
                 <?php echo errorDisplay(); ?>
@@ -34,11 +25,6 @@
                 </div>
                 <button type ="submit" class="btn btn-primary btn-block" name="reg_btn"><i class="fa fa-paper-plane"></i> Register</button>
             </form>
-            <p>Already a member?<a href=""> Login</a></p>
+            <p>Already a member?<a href="login.php"> Login</a></p>
         </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>  
-</body>
-</html>
+<?php include("footer.php"); ?>
